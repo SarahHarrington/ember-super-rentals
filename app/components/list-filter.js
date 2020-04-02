3d858cdd1ce144e79c9fa7e786bc4ed978a1cdb3
Sarah Harrington <sarah.harrington@iorahealth.com>
2020-04-02 09:44:26 -0500

    through complex components section

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -1,19 +1,29 @@
-import Component from '@ember/component';
+import Component from "@ember/component";
 
 export default Component.extend({
-  className: ['list-filter'],
-  value: '',
+  className: ["list-filter"],
+  value: "",
 
   init() {
     this._super(...arguments);
-    this.filter('').then((results) => this.set('results', results));
+    // this.filter('').then((results) => this.set('results', results));
+    this.filter("").then(allResults => {
+      this.set("results", allResults.results);
+    });
   },
 
   actions: {
     handleFilterEntry() {
       let filterInputValue = this.value;
       let filterAction = this.filter;
-      filterAction(filterInputValue).then((filterResults) => this.set('results', filterResults))
+      // filterAction(filterInputValue).then(filterResults =>
+      //   this.set("results", filterResults)
+      // );
+      filterAction(filterInputValue).then(filterResults => {
+        if (filterResults.query === this.value) {
+          this.set("results", filterResults.results);
+        }
+      });
     }
   }
 });
